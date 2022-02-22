2be352239c258e985252d375ade93eb48ff19e13
ilker Aytı <iayti@blutv.com>
2022-02-22 17:15:32 +0300

    PositionType added.

---
+++ BlazorAppCRUD/Utilities/Enums/PositionType.cs
@@ -0,0 +1,31 @@
+﻿using EnumsNET;
+using System.ComponentModel;
+
+namespace BlazorAppCRUD.Utilities.Enums
+{
+    public enum PositionType
+    {
+        [Description("Software Developer")]
+        SoftwareDeveloper,
+
+        [Description("Senior Software Developer")]
+        SeniorSoftwareDeveloper
+    }
+
+    public static class PositionTypeExtentions
+    {
+        public static List<string> GetPositionTypeDescriptions()
+        {
+            var list = Enum.GetValues(typeof(PositionType)).Cast<PositionType>().ToList();
+
+            List<string> descriptions = new List<string>();
+            foreach (var item in list)
+            {
+                descriptions.Add(item.AsString(EnumFormat.Description));
+            }
+
+            return descriptions;
+        }
+    }
+
+}
