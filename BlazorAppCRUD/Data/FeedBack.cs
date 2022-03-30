1e89321067f32a13524948bbbe8dd35ad25f0cb5
Erdem Doğanay <erdemdoganayiletisim@gmail.com>
2022-03-30 17:23:01 +0300

    feedBack complated

--- BlazorAppCRUD/Data/FeedBack.cs
+++ BlazorAppCRUD/Data/FeedBack.cs
@@ -3,6 +3,7 @@
     public class FeedBack : AuditableEntity
     {
         public int Id { get; set; }
+        public string Email { get; set; }
         public string Subject { get; set; }
         public string Message { get; set; }
 
---
+++ BlazorAppCRUD/Migrations/20220330132651_UpdateFeedBack.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorAppCRUD.Migrations
+{
+    public partial class UpdateFeedBack : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "FeedBacks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "FeedBacks");
+        }
+    }
+}
